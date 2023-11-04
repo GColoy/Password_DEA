9032efd48a885a81798918af533fc65e2cea9b9b
Felix Camrath <felix.camrath@gmail.com>
2023-11-04 18:15:41 +0100

    Testing finished

--- testing.ts
+++ testing.ts
@@ -9,12 +9,31 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Next char: ', (answer) => {
-  // Get the char value from the answer
-  const char = answer.charAt(0);
-  // Do something with the char
-  console.log('You entered: ' + char);
-  // Close the interface
+rl.question('Test a password: ', (answer) => {
+  console.log("Here we go:");
+  let currentNodeIndex = 0;
+  let works = true;
+  let finalState = false;
+  let index = 0;
+  let indexTrace: number[] = []
+  answer.split("").forEach(char => {
+    if (!works) return
+    const localIndex = Nodes[index].nextNodes.findIndex(string => string.charAt(0) == char);
+    if (localIndex == -1) {
+      console.write(" <-- cant insert a " + char + " here")
+      works = false
+      return
+    }
+    console.write(char);
+    index = Nodes.findIndex(node => node.name == Nodes[index].nextNodes[localIndex])
+    finalState = Nodes[index].finalState
+    indexTrace.push(index)
+  })
+  if (!works) console.log("\nThe passwort isn't allwed")
+  else if (!finalState) console.log("\nThe password didn't reach a final state")
+  else if (works) console.log("\nThe passwort works")
+  console.log("Trace:")
+  indexTrace.forEach(number => console.log(Nodes[number].name))
   rl.close();
 });
 
--- visualize.ts
+++ visualize.ts
@@ -3,7 +3,7 @@ import { node, sets } from "./types";
 const json = await Bun.file("output.json").text()
 const Nodes: node[] = JSON.parse(json);
 
-const NodeNames = Nodes.map(node => node.name).join(",1\n")
+const NodeNames = Nodes.map(node => node.name).join(",1\n")+",1"
 const NodeConnections = Nodes.flatMap(node => node.nextNodes.map(nextNode => [node.name, nextNode])).map(connection => connection[0] + "," + connection[1]).join("\n")
 
 await Bun.write("NodeNames.csv", NodeNames)
